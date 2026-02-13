77f28d3c4b0408d02acec450515acfdf915e7dfb
SiteD-Server <Yasser.Bishesari@gmail.com>
2026-02-13 21:55:23 +0330

    codex model migration

--- resources/views/layouts/auth/card.blade.php
+++ resources/views/layouts/auth/card.blade.php
@@ -4,14 +4,14 @@
         @include('partials.head')
     </head>
     <body class="min-h-screen bg-neutral-100 antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-        <div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-            <div class="flex w-full max-w-md flex-col">
+    <div class="bg-muted flex min-h-svh flex-col items-center justify-start md:justify-center gap-6 p-6 md:p-10">
+         <div class="flex w-full max-w-md flex-col">
                 <a href="{{ route('home') }}" class="flex flex-col items-center" wire:navigate>
                     <x-logo class="text-zinc-700 dark:text-zinc-300 h-16 pb-2 animate-pulse"/>
                 </a>
                 <div class="flex flex-col gap-6">
                     <div class="rounded-xl border bg-white dark:bg-zinc-900 dark:border-stone-800 text-stone-800 shadow-xs">
-                        <div class="px-10 py-8">{{ $slot }}</div>
+                        <div class="px-10 py-8 ">{{ $slot }}</div>
                     </div>
                 </div>
             </div>
--- routes/auth.php
+++ routes/auth.php
@@ -1,5 +1,4 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::livewire('/login', 'pages::auth.login')->name('login')->middleware('guest');
 Route::livewire('/role-select', 'pages::auth.role-select')->name('role-select')->middleware('auth');
